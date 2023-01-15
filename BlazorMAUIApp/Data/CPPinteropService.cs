0fa095d8a5f19037e809f3952187869d72950e2a
jstrom2002 <jhstrom1@gmail.com>
2023-01-15 01:58:57 -0700

    Add project files.

---
+++ BlazorMAUIApp/Data/CPPinteropService.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace BlazorMAUIApp.Data;
+
+public class CPPinteropService
+{
+    // Use 'AppDomain.CurrentDomain.BaseDirectory' to get the output directory and store
+    // your dlls there (ideally as a pre-build process). Example:
+    // '.\bin\Debug\net6.0-windows10.0.19041.0\win10-x64\AppX'
+    [DllImport("test.dll", CallingConvention = CallingConvention.Cdecl)]
+    public static extern int Diu(int a);
+
+}
---
+++ BlazorMAUIApp/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorMAUIApp;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
